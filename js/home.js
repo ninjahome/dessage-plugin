3af6120c64a3819b76c93f21c66c0997d7a206cd
basdevelop <hyperorchid@outlook.com>
2024-05-18 15:28:14 +0800

    save tmp

--- js/home.js
+++ js/home.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", initWelcomePage);
 function initWelcomePage() {
     const agreeCheckbox = document.getElementById('welcome-agree');
     const createButton = document.getElementById('welcome-create');
-    createButton.addEventListener('click', ()=>{navigateTo('#onboarding/create-password');});
+    createButton.addEventListener('click', () => {
+        navigateTo('#onboarding/create-password');
+    });
 
     // 初始状态
     createButton.disabled = !agreeCheckbox.checked;
@@ -31,25 +33,34 @@ function initWelcomePage() {
 
     const showPasswordButtons = document.querySelectorAll('.show-password');
     showPasswordButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const passwordInput = this.previousElementSibling;
-            if (passwordInput.type === 'password') {
-                passwordInput.type = 'text';
-                this.textContent = 'Hide';
-            } else {
-                passwordInput.type = 'password';
-                this.textContent = 'Show';
-            }
-        });
+        button.addEventListener('click',  showPassword);
     });
 
     window.addEventListener('hashchange', function () {
         showView(window.location.hash);
     });
 
+    if (window.location.hash === '#onboarding/recovery-phrase'){
+        const mnemonic = localStorage.getItem('mnemonic');
+        if(mnemonic){
+            displayMnemonic(mnemonic);
+        }
+    }
+
     showView(window.location.hash || '#onboarding/welcome');
 
     window.navigateTo = navigateTo;
+}
+
+function showPassword(){
+    const passwordInput = this.previousElementSibling;
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        this.textContent = 'Hide';
+    } else {
+        passwordInput.type = 'password';
+        this.textContent = 'Show';
+    }
 }
 
 function navigateTo(hash) {
@@ -69,9 +80,26 @@ function showView(hash) {
 }
 
 function createWallet() {
+    const mnemonic = bip39.generateMnemonic();
+    localStorage.setItem('mnemonic', mnemonic); // 保存到本地存储
     navigateTo('#onboarding/recovery-phrase');
+    displayMnemonic(mnemonic);
 }
 
 function importWallet() {
     // 导入钱包的逻辑
+}
+
+function displayMnemonic(mnemonic) {
+    const wordsArray = mnemonic.split(' ');
+    const mnemonicContainer = document.querySelector(".recovery-phrase-container");
+    mnemonicContainer.innerHTML = ''; // 清空以前的内容
+
+    wordsArray.forEach((word, index) => {
+        const div = document.getElementById("recovery-phrase-item-template").cloneNode(true);
+        div.style.display = 'block';
+        div.querySelector(".phrase-item-index").innerText = index + 1;
+        div.querySelector(".phrase-item-value").innerText = word;
+        mnemonicContainer.appendChild(div);
+    });
 }
