99dcaee8fbed2364d1d17218f61569200abfc722
basdevelop <hyperorchid@outlook.com>
2024-05-28 10:17:46 +0800

    save tmp

--- js/main.js
+++ js/main.js
@@ -10,12 +10,34 @@ async function initDessagePlugin() {
     testKey();
 }
 
+
+const { Buffer, Transaction } = EthereumTx;
 function testKey() {
     const priArray = hexStringToByteArray("9056dbc21a82398db5e16a5efb546c8335203dccda7ca42b6d53ba727f57db60");
     const key = new ProtocolKey(priArray);
+
     const privateKeyHex = key.ECKey.getPrivate('hex');
     console.log(priArray, 'Private Key(ethereum or btc):',
-        privateKeyHex, "btc address:", key.BtcAddr,"eth address",key.EthAddr);
+        privateKeyHex, "btc address:", key.BtcAddr, "eth address", key.EthAddr);
+
+    const privateArr = hexStringToByteArray(privateKeyHex)
+    const publicKey = key.ECKey.getPublic();
+    const publicKeyBytes = publicKey.encode('array', false);
+    console.log("privateArr=>", privateArr, "\npublicKeyBytes",publicKeyBytes);
+
+    const txParams = {
+        nonce: '0x0', // 交易计数器
+        gasPrice: '0x09184e72a000', // 每单位 gas 的价格
+        gasLimit: '0x2710', // 最大 gas 数量
+        to: '0x0000000000000000000000000000000000000000', // 接收方地址
+        value: '0x00', // 发送的以太币数量
+        data: '0x', // 交易数据
+    };
+
+    const tx = new Transaction(txParams, { chain: 'mainnet' });
+    const buffer = Buffer.from(privateKeyHex, 'hex');
+    tx.sign(buffer);
+    console.log(tx.serialize()) ;
 }
 
 function pluginClicked() {
